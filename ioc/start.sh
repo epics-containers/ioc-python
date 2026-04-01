57cb9044d96e70ca4b42f3f3d48f0bd1f591e261
Giles Knap <giles.knap@diamond.ac.uk>
2026-04-01 08:57:38 +0100

    use main.py or $MAIN_PYTHON as starup script

--- ioc/start.sh
+++ ioc/start.sh
@@ -34,11 +34,17 @@ if [ -f ${CONFIG_DIR}/start.sh ]; then
     exec bash ${CONFIG_DIR}/start.sh
 fi
 
+# default to using main.py as entry point for the IOC, but allow override with
+# MAIN_PYTHON environment variable
+MAIN_PYTHON=${MAIN_PYTHON:-main.py}
+STARTUP_PYTHON=${CONFIG_DIR}/${MAIN_PYTHON}
+export $PYTHONPATH=${CONFIG_DIR}
+
 # execute the first python file in *.py from the config directory, if it exists
-if [ -f ${CONFIG_DIR}/*.py ]; then
-    exec python ${CONFIG_DIR}/*.py
+if [ -f ${STARTUP_PYTHON} ]; then
+    exec python ${STARTUP_PYTHON}
 else
-    echo "No startup script found in ${CONFIG_DIR}"
+    echo "${STARTUP_PYTHON} not found"
     echo "Restarting in 20 seconds..."
     sleep 20
     exit 1
